10cd613f396185df4517f91d88cb1aa9a31ed408
zhal2810 <afrizalafandi3090@gmail.com>
2026-09-05 02:33:37 +0700

    chore: rotate DEFAULT_TOKEN to new wae_8c8d...

--- src/config/appDefaults.ts
+++ src/config/appDefaults.ts
@@ -1,7 +1,7 @@
 // Isi data kamu di sini biar tidak perlu input di Settings tiap buka app
 // Aman untuk pemakaian PRIVATE. JANGAN commit token asli ke repo public!
 
-export const DEFAULT_TOKEN = "wae_4b08b4d8d7f82c8624e1026f4872b817c8259e755c824273948cf418dffbf393";
+export const DEFAULT_TOKEN = "wae_8c8da10d55b563349028856baa5e1fc165feff38ddb6074e9e6520bd2cc949a7";
 export const DEFAULT_USERNAME = "zxz";
 export const DEFAULT_USER_ID = "69b6e417eb350ea7e6ec9bc6";
 
