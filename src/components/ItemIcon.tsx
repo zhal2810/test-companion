cd196cb074cefbd2ade6122f6c6e941b21aaf64a
zhal2810 <afrizalafandi3090@gmail.com>
2026-09-16 03:15:15 +0700

    fix(icon): match lowercase woodenCase case in ItemIcon fallback

--- src/components/ItemIcon.tsx
+++ src/components/ItemIcon.tsx
@@ -755,6 +755,7 @@ export default function ItemIcon({ itemCode, className = '', size = 'md' }: Item
         );
 
       case 'woodenCase': // Wooden Case
+      case 'woodencase':
         return (
           <svg viewBox="0 0 64 64" className="w-full h-full" fill="none" xmlns="http://www.w3.org/2000/svg">
             <defs>
